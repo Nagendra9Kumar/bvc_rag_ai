5412771be8436c6e334ce44bf8b4147190655085
Nagendra Gubbala <nagendragubbala999+9@gmail.com>
2025-03-23 22:28:37 +0530

    feat: add fallbackRedirectUrl to SignIn component in sign-in and sign-up pages

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -3,7 +3,7 @@ import { SignIn } from '@clerk/nextjs'
 export default function SignInPage() {
   return (
     <div className="flex min-h-screen items-center justify-center">
-      <SignIn />
+      <SignIn fallbackRedirectUrl={process.env.NEXT_PUBLIC_CLERK_AFTER_SIGN_IN_URL} />
     </div>
   )
 }
