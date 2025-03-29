622590644972abae8831216ec455bdeb2800b4bf
Nagendra Gubbala <nagendragubbala999+9@gmail.com>
2025-03-29 21:16:02 +0530

    feat: enhance admin-only component to log unauthorized access and display toast notification

--- components/admin-only.tsx
+++ components/admin-only.tsx
@@ -2,17 +2,26 @@
 
 import { useRoleAuth } from "@/hooks/useRoleAuth";
 import { ReactNode } from "react";
+import { useToast } from "./ui/use-toast";
 
 export async function AdminOnly({ children }: { children: ReactNode }) {
   const { role, isLoading } = await useRoleAuth();
   const isAdmin = role === 'admin';
-
+  const { toast } = useToast();
   if (isLoading) {
     return null;
   }
 
   if (!isAdmin) {
-    return null;
+    
+    console.log("Unauthorized access attempt to admin-only component");
+    return (
+      toast({
+        title: "Access Denied",
+        description: "You do not have administrator privileges",
+        variant: "destructive"
+      })
+    )
   }
 
   return <>{children}</>;
